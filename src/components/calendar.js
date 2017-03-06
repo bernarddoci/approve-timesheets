ff12f211b02a9a8d8048415158c94e49209b7a0d
Bernard Doci <Bernard Doci>
2017-03-06 11:49:38 +0100

    Month state moved to Calendar Component

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -7,14 +7,28 @@ class Calendar extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			month: '',
+			month: new Date().getMonth()
 		};
+		this.nextMonth = this.nextMonth.bind(this);
+		this.prevMonth = this.prevMonth.bind(this);
+	}
+
+	nextMonth(){
+		this.setState({month: this.state.month <= 10? this.state.month + 1: 11});
+	}
+
+	prevMonth(){
+		this.setState({month: this.state.month >= 1? this.state.month - 1: 0});
 	}
 
 	render(){
+		console.log(this.state.month);
 		return (
 			<div className="calendar">
-				<Month />
+				<Month 
+					nextMonth={this.nextMonth} 
+					prevMonth={this.prevMonth} 
+					month={this.state.month}/>
 				<DaysHeader />
 				<Days />
 			</div>);
--- src/components/month.js
+++ src/components/month.js
@@ -5,35 +5,17 @@ class Month extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			months: [],
-			month: ''
+			months: ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"]
 		}
-		this.prevMonth = this.prevMonth.bind(this);
-		this.nextMonth = this.nextMonth.bind(this);
-	}
-
-	prevMonth(){
-		this.setState({month: this.state.month >= 1? this.state.month - 1: 0});
-	}
-
-	nextMonth(){
-		this.setState({month: this.state.month <= 10? this.state.month + 1: 11});
-	}
-
-	componentDidMount(){
-		this.setState({
-			months: [ "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December" ],
-    		month: new Date().getMonth()
-		});
 	}
 
 	render(){
 		return (
 			<div className="month">
-				<button onClick={this.prevMonth}>Prev</button>
-				<h3>{this.state.months[this.state.month]}</h3>
-				<button onClick={this.nextMonth}>Next</button>
+				<button onClick={this.props.prevMonth}>Prev</button>
+				<h3>{this.state.months[this.props.month]}</h3>
+				<button onClick={this.props.nextMonth}>Next</button>
 			</div>);
 	}
 }
